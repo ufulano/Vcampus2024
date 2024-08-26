465b969b7b9ef49b4250bd8df81c5e3b31b3dbd8
fishchoker <1191556590@qq.com>
2024-08-26 17:57:50 +0800

    提供错误描述，尝试实现登录验证

--- Vcampus2024/src/tech/client/login/LoginVerify.java
+++ Vcampus2024/src/tech/client/login/LoginVerify.java
@@ -1,5 +1,42 @@
 package tech.client.login;
 
+import tech.connection.*;
+
+/*
+ * 登录界面相关功能
+ * 身份验证和判断
+ */
 public class LoginVerify {
 
+	public static void verify(String cardNumber, String password) {
+		
+		
+		System.out.println("Verifing...");
+		//用于建立连接和消息
+		Message message= new Message(cardNumber,password,"192.168.101.210", 8323);
+		System.out.println(message);
+		SocketClientWorker connection=new SocketClientWorker(message);
+		connection.SendMessage();
+		return;
+	}
+	/*public static Student verifyStudent(String cardNumber, String password) {
+		return ResponseUtils
+				.getResponseByHash(new Request(App.connectionToServer, null, "tech.zxuuu.server.auth.Auth.studentLoginChecker",
+						new Object[] { new Student(cardNumber, null, OtherUtils.getMD5(password), null) }).send())
+				.getReturn(Student.class);
+	}
+
+	public static Teacher verifyTeacher(String cardNumber, String password) {
+		return ResponseUtils
+				.getResponseByHash(new Request(App.connectionToServer, null, "tech.zxuuu.server.auth.Auth.teacherLoginChecker",
+						new Object[] { new Student(cardNumber, null, OtherUtils.getMD5(password), null) }).send())
+				.getReturn(Teacher.class);
+	}
+
+	public static Manager verifyManager(String cardNumber, String password) {
+		return ResponseUtils
+				.getResponseByHash(new Request(App.connectionToServer, null, "tech.zxuuu.server.auth.Auth.managerLoginChecker",
+						new Object[] { new Student(cardNumber, null, OtherUtils.getMD5(password), null) }).send())
+				.getReturn(Manager.class);
+	}*/
 }
